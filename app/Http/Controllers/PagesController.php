98d34cd17b41516dfc5499660fbc1fc3586bc24f
DESKTOP-NR0C68M\Xin <ezekhiel1337@gmail.com>
2019-04-18 22:27:09 +0800

    Added fillable in page model || added functions in PagesController || added 2 view files

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,6 +7,9 @@ use App\Page;
 
 class PagesController extends Controller
 {
+
+
+
    public function page(Request $request, Page $page)
    {
       // $page = Page::whereSlug($slug)
@@ -14,4 +17,65 @@ class PagesController extends Controller
       
        return view('Pages.Static', compact('page'));
    }
+
+   public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
--- app/Page.php
+++ app/Page.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Page extends Model
 {
+    protected $fillable = ['title', 'slug', 'content'];
+
     public function getRouteKeyName()
     {
         return 'slug';
